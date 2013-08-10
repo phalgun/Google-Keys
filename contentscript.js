572560922a5bed12eeacaf9960615e528915eead
Phalgun Guduthur <phalgun_g@yahoo.com>
2013-08-10 21:00:06 +0530

    Extension now supports all links in the Google search result page using Ctrl+Shift+<number> for background tab and Alt+Shift+<number> for opening in current tab. Deleted unnecessary code and removed background.js

--- contentscript.js
+++ contentscript.js
@@ -1,29 +1,62 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+
+function setupKeyBindings() {
+
+	// using string concatenation and a loop oddly doesn't seem to work.	
+
+	Mousetrap.bind('ctrl+shift+1', function () {openNewBackgroundTab(1)});
+	Mousetrap.bind('alt+shift+1', function () {openNewBackgroundTab(1, false)});
+
+	Mousetrap.bind('ctrl+shift+2', function () {openNewBackgroundTab(2)});
+	Mousetrap.bind('alt+shift+2', function () {openNewBackgroundTab(2, false)});
+
+	Mousetrap.bind('ctrl+shift+3', function () {openNewBackgroundTab(3)});
+	Mousetrap.bind('alt+shift+3', function () {openNewBackgroundTab(3, false)});
+
+	Mousetrap.bind('ctrl+shift+4', function () {openNewBackgroundTab(4)});
+	Mousetrap.bind('alt+shift+4', function () {openNewBackgroundTab(4, false)});
+
+	Mousetrap.bind('ctrl+shift+5', function () {openNewBackgroundTab(5)});
+	Mousetrap.bind('alt+shift+5', function () {openNewBackgroundTab(5, false)});
+
+	Mousetrap.bind('ctrl+shift+6', function () {openNewBackgroundTab(6)});
+	Mousetrap.bind('alt+shift+6', function () {openNewBackgroundTab(6, false)});
+
+	Mousetrap.bind('ctrl+shift+7', function () {openNewBackgroundTab(7)});
+	Mousetrap.bind('alt+shift+7', function () {openNewBackgroundTab(7, false)});
+
+	Mousetrap.bind('ctrl+shift+8', function () {openNewBackgroundTab(8)});
+	Mousetrap.bind('alt+shift+8', function () {openNewBackgroundTab(8, false)});
+
+	Mousetrap.bind('ctrl+shift+9', function () {openNewBackgroundTab(9)});
+	Mousetrap.bind('alt+shift+9', function () {openNewBackgroundTab(9, false)});
+
+}
 
 
-Mousetrap.bind('ctrl+shift+1', function () {openNewBackgroundTab(1)}, 'keyup');
+function openNewBackgroundTab(linkNumber, background) {
 
-function openNewBackgroundTab(linkNumber){
-
-	var allSearchResults = '//h3[@class="r"]/a/@href';
-	xpathResult = document.evaluate( allSearchResults,  document, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null );
-
-	// alert(xpathResult.resultType + "|" );
-
-	var searchLink = xpathResult.iterateNext();
-	for (var x = 1; x <= linkNumber; x++) {
-		searchLink = xpathResult.iterateNext();
-	}
-	
+	background = typeof background !== 'undefined' ? background : true;
 	var a = document.createElement("a");
-    a.href = searchLink.value;
+    a.href = getNthLink(linkNumber);
     var evt = document.createEvent("MouseEvents");
     //the tenth parameter of initMouseEvent sets ctrl key
     evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0,
-                                true, false, false, false, 0, null);
+                                background, false, false, false, 0, null);
     a.dispatchEvent(evt);
 }
 
-// chrome.extension.sendRequest(payload, function(response) {});
+// TODO : Use better XPATH to find the needed link directly instead of looping
+function getNthLink(linkNumber) {
+	var allSearchResults = '//h3[@class="r"]/a/@href';
+	xpathResult = document.evaluate( allSearchResults,  document, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null );
+
+
+	var searchLink = xpathResult.iterateNext();
+	for (var x = 1; x < linkNumber; x++) {
+		searchLink = xpathResult.iterateNext();
+	}
+
+	return searchLink.value;
+}
+
+setupKeyBindings();
