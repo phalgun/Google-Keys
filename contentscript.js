6f5e32651780313f0e61f22c0db685f0ea51f035
Phalgun Guduthur <phalgun_g@yahoo.com>
2013-08-10 19:35:36 +0530

    extension now has support for opening the first link in the background using the keyboard shortcut ctrl+shift+1

--- contentscript.js
+++ contentscript.js
@@ -2,22 +2,23 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-var first_search_result = '//h3[@class="r"]/a/@href';
-xpathResult = document.evaluate( first_search_result,  document, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null );
 
-// alert(xpathResult.resultType + "|" );
-var searchLink = xpathResult.iterateNext();
-while(searchLink) {
-	// alert(searchLink.value);
-	// searchLink = xpathResult.iterateNext();
-	// window.open(searchLink.value, "_blank");
-	openNewBackgroundTab(searchLink.value);
-	break;
-}
+Mousetrap.bind('ctrl+shift+1', function () {openNewBackgroundTab(1)}, 'keyup');
 
-function openNewBackgroundTab(link){
-    var a = document.createElement("a");
-    a.href = link;
+function openNewBackgroundTab(linkNumber){
+
+	var allSearchResults = '//h3[@class="r"]/a/@href';
+	xpathResult = document.evaluate( allSearchResults,  document, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null );
+
+	// alert(xpathResult.resultType + "|" );
+
+	var searchLink = xpathResult.iterateNext();
+	for (var x = 1; x <= linkNumber; x++) {
+		searchLink = xpathResult.iterateNext();
+	}
+	
+	var a = document.createElement("a");
+    a.href = searchLink.value;
     var evt = document.createEvent("MouseEvents");
     //the tenth parameter of initMouseEvent sets ctrl key
     evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0,
