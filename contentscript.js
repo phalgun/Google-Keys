955bdf22f858758735833d04bc0461056c769ab5
Phalgun Guduthur <phalgun_g@yahoo.com>
2013-08-10 18:44:59 +0530

    opens the first link in the background w/o any user intervention

--- contentscript.js
+++ contentscript.js
@@ -5,13 +5,24 @@
 var first_search_result = '//h3[@class="r"]/a/@href';
 xpathResult = document.evaluate( first_search_result,  document, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null );
 
-alert(xpathResult.resultType + "|" );
+// alert(xpathResult.resultType + "|" );
 var searchLink = xpathResult.iterateNext();
 while(searchLink) {
-	alert(searchLink.value);
-	searchLink = xpathResult.iterateNext();
+	// alert(searchLink.value);
+	// searchLink = xpathResult.iterateNext();
+	// window.open(searchLink.value, "_blank");
+	openNewBackgroundTab(searchLink.value);
+	break;
 }
 
+function openNewBackgroundTab(link){
+    var a = document.createElement("a");
+    a.href = link;
+    var evt = document.createEvent("MouseEvents");
+    //the tenth parameter of initMouseEvent sets ctrl key
+    evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0,
+                                true, false, false, false, 0, null);
+    a.dispatchEvent(evt);
+}
 
-var payload = xpathResult;
 // chrome.extension.sendRequest(payload, function(response) {});
